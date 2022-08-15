b9429e62c9887adea81e5498d7c9c68d08300076
Devin Stompanato <devin@rastomp.com>
2022-08-14 20:43:03 -0500

    added program installs

--- init.sh
+++ init.sh
@@ -5,25 +5,42 @@
 ###########################################################################
 
 
+rc_file="~/.bashrc"
+
 doOrEcho() {
   $@ 2> /dev/null || echo '[ '$@' ] not needed'
 }
 
 searchOrAdd() {
-  grep $1 ~/.bashrc || echo $1 >> ~/.bashrc
+  # Arg 1 = Command to add
+  grep $1 $rc_file || echo $1 >> $rc_file
 }
 
 script=`readlink -f $0`
 script_dir=`dirname $script`
 
-read -p "Please enter a name for this session: " serverName
+read -p "Install base programs? (y/n)" installBase
+if [[ $installBase == "y" ]]; then
+  sudo pacman -Syu cowsay lolcat vim neofetch cmatrix git tmux htop bpytop tig docker code tree
+  sudo pacman -Syu libreoffice-fresh thunderbird obs-studio
+else
+  echo "Skipping install"
+fi
 
-echo "=========================="
-echo "==== Install starting ===="
-echo "=========================="
+read -p "Install zsh and set as default shell? (y/n)" installZsh
+if [[ $installZsh == "y" ]]; then
+  rc_file="~/.zshrc"
+
+  sudo pacman -Syu zsh
+  sudo chsh -s /bin/zsh $(whoami)
+
+  echo "Restart your session to finalize"
+else
+  echo "Skipping zsh install"
+fi
 
 # Prepare tmux conf and bashrc
-echo -e "\n-> Preparing bashrc and vimrc ..."
+echo -e "\nPreparing rc files ..."
 cp ${script_dir}/basic-configs/vimrc ~/.vimrc
 cp ${script_dir}/basic-configs/tmux.conf ~/.tmux.conf
 
@@ -31,6 +48,9 @@ cp ${script_dir}/basic-configs/tmux.conf ~/.tmux.conf
 searchOrAdd "export TERM=screen-256color"
 searchOrAdd "alias taily='tail -f -n 5000'"
 searchOrAdd "alias yeet='rm -rf'"
+searchOrAdd "alias sshls='echo \"TODO\" | cowsay | lolcat'"
+searchOrAdd "alias gs='git status'"
+searchOrAdd "alias gd='git diff'"
 
 # Create necessary directories for plugins.
 echo -e "\n-> Installing scala syntax highlighting ..."
@@ -49,22 +69,16 @@ echo -e "\n-> Installing plugins ...\n"
 cd ~/.vim/bundle
 doOrEcho git clone https://github.com/scrooloose/nerdtree
 doOrEcho git clone https://github.com/scrooloose/nerdcommenter
-doOrEcho git clone https://github.com/tomasr/molokai
 doOrEcho git clone https://github.com/ajmwagar/vim-deus.git
 doOrEcho git clone https://github.com/bronson/vim-trailing-whitespace
 doOrEcho git clone https://github.com/bling/vim-airline
 
 # copy configs/color schemes into vim folders
-doOrEcho cp ~/.vim/bundle/molokai/colors/molokai.vim ~/.vim/colors/
 doOrEcho cp ~/.vim/bundle/vim-deus/colors/deus.vim ~/.vim/colors/
 doOrEcho cp ~/.vim/bundle/vim-airline/autoload/airline.vim ~/.vim/autoload/
 
 # Execute install for vim plugins
 vim +PlugInstall +qall
-
-# Restores backup for any subsequent runs
-echo -e "\n-> Cleaning up ...\n"
-mv ${script_dir}/basic-configs/bashrc.bak ${script_dir}/basic-configs/bashrc
 
 echo && echo "=========================="
         echo "==== Install complete ===="
